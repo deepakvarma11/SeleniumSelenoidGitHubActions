5eb89c0e1bc4c2eb4b2911b7334211bd7dfa75a7
Saidinesh Narisetti <19299232+saidineshnarisetti@users.noreply.github.com>
2023-06-09 11:59:42 +0530

    Update GooglePage.java

--- src/test/java/Test/MavenProject/GooglePage.java
+++ src/test/java/Test/MavenProject/GooglePage.java
@@ -7,6 +7,7 @@ import org.testng.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import java.net.URL;
 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -19,7 +20,8 @@ public class GooglePage {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 	options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+	URL u=new URL("http://localhost:4444/wd/hub");
+        driver = new ChromeDriver(u, options);
     }
     @Test
     void GoogleTitleValidation() {
