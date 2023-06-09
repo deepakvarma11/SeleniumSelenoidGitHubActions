cd0cdb4d26e6371555dfadab3f4b3ca88b7b5237
Saidinesh Narisetti <saidinesh.narisetti@thoughtworks.com>
2023-06-09 12:06:50 +0530

    Upadating google page

--- src/test/java/Test/MavenProject/GooglePage.java
+++ src/test/java/Test/MavenProject/GooglePage.java
@@ -7,21 +7,25 @@ import org.testng.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
 import java.net.URL;
 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class GooglePage {
-	public WebDriver driver;
+	
+	public static RemoteWebDriver driver=null;
 	@BeforeTest
-    void WebDriversetup() {
+    void WebDriversetup() throws MalformedURLException {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-	options.addArguments("--headless");
-	URL u=new URL("http://localhost:4444/wd/hub");
-        driver = new ChromeDriver(u, options);
+        options.addArguments("--headless");
+        URL u =new URL("http://localhost:4444/wd/hub");
+        driver = new RemoteWebDriver (u,options);
     }
     @Test
     void GoogleTitleValidation() {
